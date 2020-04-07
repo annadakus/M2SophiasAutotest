87f184b81b9bad6d42ad1e7a397ac039d8a906f4
annadakus <anyadakus@gmail.com>
2020-04-07 18:09:47 +0300

    Update LoggedInCheckoutTest.java

--- src/test/java/com/sophiasstyle/m2/tests/LoggedInCheckoutTest.java
+++ src/test/java/com/sophiasstyle/m2/tests/LoggedInCheckoutTest.java
@@ -1,24 +1,24 @@
-package com.sophiasstyle.m2.tests;
+// package com.sophiasstyle.m2.tests;
 
-import com.sophiasstyle.m2.page_factory.HomepagePage;
-import org.junit.Test;
+// import com.sophiasstyle.m2.page_factory.HomepagePage;
+// import org.junit.Test;
 
-public class LoggedInCheckoutTest extends HomepagePage {
+// public class LoggedInCheckoutTest extends HomepagePage {
 
-    @Test
-    public void loggedInCheckoutTest() throws InterruptedException{
-        openHomePage();
-        goToLoginPage();
-        login("anna@ecomitize.com", "1111abcd1111Q");
-        checkUserIsLoggedIn();
-        goToCategoryPage();
-        goToSecondProductPage();
-        addProductToCart();
-        goToShoppingCart();
-        checkShoppingCart();
-        goToCheckoutPage();
-        fillOutShippingInfo("Anna", "Test",
-                "7 Test street", "Test", "Nebraska", "68138",
-                "United States", "123456789");
-    }
-}
+//     @Test
+//     public void loggedInCheckoutTest() throws InterruptedException{
+//         openHomePage();
+//         goToLoginPage();
+//         login("anna@ecomitize.com", "1111abcd1111Q");
+//         checkUserIsLoggedIn();
+//         goToCategoryPage();
+//         goToSecondProductPage();
+//         addProductToCart();
+//         goToShoppingCart();
+//         checkShoppingCart();
+//         goToCheckoutPage();
+//         fillOutShippingInfo("Anna", "Test",
+//                 "7 Test street", "Test", "Nebraska", "68138",
+//                 "United States", "123456789");
+//     }
+// }
